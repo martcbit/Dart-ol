0faf20a278d6f9c759128c407c32176e5203bfca
martcbit <martins.allysson@hotmail.com>
2021-05-01 23:31:32 -0300

    c-1

---
+++ dart _exercicios/oo/class_data.dart
@@ -0,0 +1,22 @@
+class Data {
+  int dia = 0;
+  int mes = 0;
+  int ano = 0;
+}
+
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 1;
+  dataAniversario.mes = 12;
+  dataAniversario.ano = 2021;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 11;
+  dataCompra.mes = 08;
+  dataCompra.ano = 2021;
+
+  print(
+      "Data do Aniversário: ${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print(
+      "Data da Compra: ${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
