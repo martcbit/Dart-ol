5222226f69cb369c47b2e20c162e29d8cf200eae
martcbit <martins.allysson@hotmail.com>
2021-05-08 00:05:50 -0300

    oo-interm-1.3.2-f

--- dart _exercicios/oo/class_static_fin_const.dart
+++ dart _exercicios/oo/class_static_fin_const.dart
@@ -2,9 +2,13 @@ class Valores {
   static int vezesClicado = 0;
 }
 
+class Pessoa {}
+
 void main() {
   Valores.vezesClicado = 2;
 
   const numero = 3;
   print(numero);
+
+  final Pessoa pessoa1 = Pessoa();
 }
